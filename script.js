5d506b29cf473707ce38da0e96fe301c28622cdb
Gabriel Rodrigues Santos <80860021+Rodriguestensor@users.noreply.github.com>
2021-10-15 14:42:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// //tipos primitivos
+
+// //boolean
+// var vOuF= false;
+// console.log(typeof(vOuF));
+
+// //number
+// var numeroqualquer = 1;
+// console.log(typeof(numeroqualquer));
+
+// //string
+
+// var nome = 'gabriel';
+
+// console.log(typeof(nome));
+
+// var escopoglobal='global';
+// console.log(escopoglobal);
+
+// function escopolocal(){
+//     let escopointerno='local';
+//     console.log(escopointerno);
+// }
+
+// escopolocal();
